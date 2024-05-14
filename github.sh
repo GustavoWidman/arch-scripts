4ca0bf48493147a52abb01e3aa97d9768f7a7864
GustavoWidman (laptop-arch) <gustavo.widman@sou.inteli.edu.br>
2024-05-14 11:44:26 -0300

    add: github and ssh patch scripts

---
+++ github.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Check if the script is run as root (sudo)
+if [ "$(id -u)" == "0" ]; then
+    echo "This script musn't be run as root. Please run as your normal user."
+    exit 1
+fi
+
+# prompt the user if he wants to make a new SSH key
+while true; do
+	read -p "Do you want to generate a new SSH key? (y/n): " -n 1 GENERATE_SSH_KEY
+	echo
+
+	# Check if the input is valid
+	if [[ $GENERATE_SSH_KEY =~ ^[YyNn]$ ]]; then
+		echo
+		break
+	else
+		echo "Invalid input. Please enter 'y' or 'n'"
+	fi
+done
+
+if [[ $GENERATE_SSH_KEY =~ ^[Yy]$ ]]; then
+	SSH_PUB_KEY=$(RUNNING_NESTED="true" ./ssh-generate.sh 2>&1 >/dev/tty)
+else
+	# prompt to select the SSH key from the list
+	echo "Select the SSH public key to use for GitHub:"
+	select SSH_PUB_KEY in $(find ~/.ssh -type f -name "*.pub"); do
+		if [ -z "$SSH_PUB_KEY" ]; then
+			echo "Invalid selection. Please try again."
+			continue
+		fi
+
+		echo "Selected public key: $SSH_PUB_KEY"
+		break
+	done
+fi
+
+read -p "Enter the GitHub username: " GITHUB_USERNAME
+
+if [ -z "${GITHUB_USERNAME}" ]; then
+	echo "GitHub username cannot be empty."
+	exit 1
+fi
+
+git config --global user.name "$GITHUB_USERNAME"
+
+read -p "Enter the GitHub email: " GITHUB_EMAIL
+
+if [ -z "${GITHUB_EMAIL}" ]; then
+	echo "GitHub email cannot be empty."
+	exit 1
+fi
+
+git config --global user.email "$GITHUB_EMAIL"
+
+git config --global gpg.format ssh
+git config --global user.signingkey "$SSH_PUB_KEY"
+git config --global commit.gpgsign true
+git config --global url."git@github.com:".insteadOf "https://github.com/"
---
+++ ssh-generate.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Check if the script is run as root (sudo)
+if [ "$(id -u)" == "0" ]; then
+    echo "This script musn't be run as root. Please run as your normal user."
+    exit 1
+fi
+
+# check if xclip is installed
+if [[ $RUNNING_NESTED != "true" ]] && ! command -v xclip &> /dev/null; then
+    echo "xclip is not installed. Installing..."
+    sudo pacman -Sy xclip --noconfirm
+    echo ""
+fi
+
+# Ask for the SSH key name
+echo -n "Enter the name of the SSH key (default: id_ed25519): "
+read SSH_KEY_NAME
+
+# If user simply pressed Enter, use the default path
+if [ -z "${SSH_KEY_NAME}" ]; then
+    SSH_KEY_NAME="id_ed25519"
+fi
+
+SSH_KEY_PATH="$HOME/.ssh/$SSH_KEY_NAME"
+
+# Generate the SSH Key
+ssh-keygen -t ed25519 -b 4096 -f "$SSH_KEY_PATH" -N ""
+
+echo ""
+echo ""
+echo "SSH public key copied to clipboard!"
+echo "You can configure your github account using the link below:"
+echo "https://github.com/settings/keys"
+
+# echo the ssh pubkey content into stderr if stderr is not a tty without using cat
+if [[ $RUNNING_NESTED == "true" ]] then
+    >&2 echo "$SSH_KEY_PATH.pub"
+    exit
+else
+    xclip -sel clip < "$SSH_KEY_PATH.pub"
+    exit
+fi
